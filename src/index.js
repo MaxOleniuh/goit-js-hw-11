5fc797ff8bd87fd69896159277a388135b55d9f1
oleniuh <maxim.oleniukh@gmail.com>
2023-04-10 23:43:35 +0100

    simple light box needs fix

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import fetchImages from './fetch.js';
 const input = document.querySelector('input');
 const form = document.querySelector('form#search-form');
 const imagesBox = document.querySelector('.gallery');
+
 const renderImages = images => {
   const { webformatURL, tags, likes, views, comments, downloads } = images;
   return `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <a><div class="info">
+   <a> <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+    <div class="info">
     <p class="info-item">
       <b>Likes</b> ${likes}
     </p>
@@ -24,7 +25,6 @@ const renderImages = images => {
       <b>Downloads</b> ${downloads}
     </p>
   </div>
-  </a>
 </div>`;
 };
 const searchImages = e => {
@@ -55,4 +55,29 @@ const searchImages = e => {
       );
     });
 };
+
+function pictureClickHandler(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+         <img width="1400" height="900" src="${e.target.src}">
+`,
+    {
+      onClose: instance => {
+        document.removeEventListener('keydown', pressedEscKeyHandler);
+      },
+    }
+  );
+  instance.show();
+  document.addEventListener('keydown', pressedEscKeyHandler);
+  function pressedEscKeyHandler(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
 form.addEventListener('submit', searchImages);
+imagesBox.addEventListener('click', pictureClickHandler);
