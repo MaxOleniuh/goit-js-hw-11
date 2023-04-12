7eff09a4b67729fd54f7d185761e68f512ef66c7
oleniuh <maxim.oleniukh@gmail.com>
2023-04-12 12:25:57 +0100

    axios error handling added

--- src/fetch.js
+++ src/fetch.js
@@ -3,5 +3,7 @@ export default function fetchImages(q, page) {
   const URL = 'https://pixabay.com/api/';
   const API_KEY = '35259660-b4522dcf8b654a7c371bdf25f';
   const FIELDS = `?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
-  return axios.get(`${URL}${FIELDS}`);
+  return axios.get(`${URL}${FIELDS}`).catch(error => {
+    console.log(error.toJSON());
+  });
 }
